bcc7e44494d048cd860ebba22c96c2e00074ee55
Ion <neagoe057@gmail.com>
2018-02-03 22:37:02 +0200

    Server setup for Heroku.

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ import expressLog from "express-bunyan-logger";
 const app = express();
 const log = bunyan.createLogger( { name: "server" } );
 
+app.set( "port", ( process.env.PORT || 8080 ) );
+
 app.use( expressLog() );
 app.use( expressLog.errorLogger() );
 
 app.use( bodyParser.urlencoded( { extended: false } ) );
 app.use( bodyParser.json() );
 
-app.listen(8080);
+app.use( express.static( __dirname + "/pubic" ) );
+
+app.listen( app.get( "port" ), () => {
+    console.log( `Server running on port ${ app.get( "port" ) }` );
+} );
