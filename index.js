8ac214358b906cc77be6a699f4d2d78371c37ad8
Ion <neagoe057@gmail.com>
2018-02-03 23:30:13 +0200

    Server ready for SPA deploy.

--- index.js
+++ index.js
@@ -8,13 +8,16 @@ const log = bunyan.createLogger( { name: "server" } );
 
 app.set( "port", ( process.env.PORT || 8080 ) );
 
-app.use( expressLog() );
-app.use( expressLog.errorLogger() );
+//app.use( expressLog() );
+//app.use( expressLog.errorLogger() );
 
 app.use( bodyParser.urlencoded( { extended: false } ) );
 app.use( bodyParser.json() );
 
 app.use( express.static( __dirname + "/pubic" ) );
+
+app.get( "*", ( req, res ) => res.sendFile( __dirname + "/public/index.html" ) );
+
 
 app.listen( app.get( "port" ), () => {
     console.log( `Server running on port ${ app.get( "port" ) }` );
