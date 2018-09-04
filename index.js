3ae2cfd04c2361252c3809f6af0b273b6946722f
Ionut Neagoe <neagoe057@gmail.com>
2018-09-04 18:00:19 +0300

    CORS

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ const log = bunyan.createLogger( { name: 'server' } );
 const PORT      = process.env.PORT || 8080;
 const PUBLIC    = __dirname + '/public';
 
-
+app.use( ( req, res, next ) => {
+    res.header( 'Access-Control-Allow-Origin', '*' ); 
+    res.header( 'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept' );
+    next();
+} );
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded( { extended: true } ) );
 app.use( express.static( PUBLIC ) );
