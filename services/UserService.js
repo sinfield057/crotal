ad37b8a3bcd87b0c9e3bef43ea0008279c9879cd
Ionut Neagoe <neagoe057@gmail.com>
2018-09-05 03:36:11 +0300

    -

--- services/UserService.js
+++ services/UserService.js
@@ -101,10 +101,11 @@ exports.ActivateUser = ( id ) => {
     return new Promise( ( resolve, reject ) => {
         User.findOneAndUpdate( { _id: id }, { activated: true }, { new: true } )
             .then( user => {
-                const payload = {
-                    id:     user._id,
-                    email:  user.email
-                };
+                if ( !user ) {
+                    reject( { status: 404, message: 'Invalid id.' } );
+                    return;
+                }
+
                 user.generateAuthToken()
                         .then( token => resolve( { status: 201, message: 'Login successful.', user: user, token } ) )
                         .catch( err => reject( { status: 500, message: 'Internal server error.' } ) );
